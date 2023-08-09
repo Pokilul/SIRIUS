e5e0204d73d245f74bfd4633cc832540b580a58b
Pakoo <franciscojavierledesmaponce@gmail.com>
2023-08-08 18:06:31 -0600

    Navigation Drawer update and app update

--- sirius/src/App.vue
+++ sirius/src/App.vue
@@ -1,24 +1,45 @@
 <template>
-  <v-app>
-    <v-row class="pb-14"> <NavBar /></v-row>
-    <v-main fluid>
-      <v-row no-gutters>
-        <v-col cols="4" xs="3" sm="3" md="4" lg="4" xl="4">
-          <v-container fluid><NavDrawer /></v-container
-        ></v-col>
-        <v-col cols="8" xs="9" sm="9" md="8" lg="8" xl="8">
-          <v-container class="mt-n16 ml-0 mr-0 pl-0 pr-auto" fluid
-            ><router-view
-          /></v-container>
-        </v-col>
-      </v-row>
-    </v-main>
-    <v-footer app><Footer /></v-footer>
-  </v-app>
+    <!-- The main app container -->
+    <v-app>
+        <!-- The navigation bar component -->
+        <NavBar @toggleDrawer="drawerOpen = !drawerOpen" @openDrawer="drawerOpen = true" />
+        <!-- The footer component -->
+        <v-footer app>
+            <Footer />
+        </v-footer>
+        <!-- The navigation drawer component -->
+        <v-navigation-drawer app width="450" :style="{ top: '150px' }" v-if="drawerOpen" @mouseleave="drawerOpen = false">
+            <Menu :menuItems="menuData" />
+        </v-navigation-drawer>
+        <!--<NavDrawer />-->
+
+        <!-- The main content area -->
+        <v-main class="w-100 mx-auto my-auto pa-auto pt-0">
+            <v-container>
+                <!-- The router view component -->
+                <router-view :style="{ top: '128px' }" />
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
 
 <script setup>
-import NavDrawer from "/src/components/NavDrawer.vue";
+import {
+    ref
+} from 'vue';
 import NavBar from "/src/components/NavBar.vue";
 import Footer from "/src/components/Footer.vue";
+import Menu from "/src/components/Menu.vue";
+// import NavDrawer from "/src/components/NavDrawer.vue";
+
+// Import the menuData object from the menuData.js file
+import { menuData } from "/src/components/menuData.js";
+
+// Define a reactive variable to control the state of the navigation drawer
+const drawerOpen = ref(true);
+
+// Define a function to toggle the state of the navigation drawer
+const toggleDrawer = () => {
+    drawerOpen.value = !drawerOpen.value;
+};
 </script>
